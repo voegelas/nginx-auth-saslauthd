9e31c2c9cdf0e532a6d448dc40b1b272080fd748
Andreas Voegele <voegelas@cpan.org>
2017-11-24 07:20:58 +0100

    Add App::nginx_auth_saslauthd.

---
+++ lib/App/nginx_auth_saslauthd.pm
@@ -0,0 +1,21 @@
+package App::nginx_auth_saslauthd;
+
+our $VERSION = '0.004';
+
+1;
+__END__
+
+=head1 NAME
+
+App::nginx_auth_saslauthd - Verify web users with Basic authentication and saslauthd
+
+=head1 DESCRIPTION
+
+See L<nginx-auth-saslauthd>. The module L<App::nginx_auth_saslauthd> is just a
+placeholder for CPAN.
+
+=head1 SEE ALSO
+
+L<nginx-auth-saslauthd>.
+
+=cut
